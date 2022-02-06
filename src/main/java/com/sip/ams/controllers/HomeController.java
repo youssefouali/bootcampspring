75ec45e22baf7def56e6dc08e7d241d41108ab94
youssefouali <youssef.ouali@esprit.tn>
2022-02-06 16:38:50 +0100

    commit 10000

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,16 +1,35 @@
 package com.sip.ams.controllers;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/info")
-	public String info()
+	public String info(Model model)
 	{
-		System.out.println("méthode affichage");
-		return "home/info";
+		ArrayList<String> names = new ArrayList<>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("Spring");
+		model.addAttribute("names", names);
+		String formation = "Fullstack";
+		System.out.println("Méthode info");
+		
+		
+		model.addAttribute("workshop",formation);
+		
+		return "home/info";  // le nom de la vue sous template /home
+		
+		
+		
 	}
 	
 	@RequestMapping("/affichage")
