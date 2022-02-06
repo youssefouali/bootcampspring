5d7ee074cb4f7ade8ed185704762d2bc1e913bf6
Youssef <48011744+youssefouali@users.noreply.github.com>
2022-02-06 14:41:55 +0100

    first project spring mvc

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/info")
+	public String info()
+	{
+		return "home/info";
+	}
+	
+	@RequestMapping("/affichage")
+	public String affichage()
+	{
+		System.out.println("méthode info");
+		return "home/affichage";
+	}
+	
+	
+
+}
